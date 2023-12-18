edc6e0d129a244c2e0ff8dcfbe9f9d3f0466a4e1
EdgeNeko <edgeneko@aiursoft.com>
2023-12-18 14:22:32 +0800

    Use abstract OCR service

--- app/Services/__init__.py
+++ app/Services/__init__.py
@@ -1,7 +1,28 @@
 from .transformers_service import TransformersService
 from .vector_db_context import VectorDbContext
-from .ocr_service import OCRService
+from ..config import config, environment
 
 transformers_service = TransformersService()
 db_context = VectorDbContext()
-ocr_service = OCRService()
+ocr_service = None
+
+if environment.local_indexing:
+    match config.ocr_search.ocr_module:
+        case "easyocr":
+            from .ocr_services import EasyOCRService
+
+            ocr_service = EasyOCRService()
+        case "easypaddleocr":
+            from .ocr_services import EasyPaddleOCRService
+
+            ocr_service = EasyPaddleOCRService()
+        case "paddleocr":
+            from .ocr_services import PaddleOCRService
+
+            ocr_service = PaddleOCRService()
+        case _:
+            raise NotImplementedError(f"OCR module {config.ocr_search.ocr_module} not implemented.")
+else:
+    from .ocr_services import DisabledOCRService
+
+    ocr_service = DisabledOCRService()
