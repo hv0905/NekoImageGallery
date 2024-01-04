1bee16cde59c9d1375344839f88f59c5d36c3f62
pk5ls20 <pk5ls20@outlook.com>
2024-01-05 03:56:22 +0800

    Keep the initial preprocessing and adjust the `EasyPaddleOCR` preprocessing to include only RGB conversion.

--- app/Services/ocr_services.py
+++ app/Services/ocr_services.py
@@ -18,10 +18,11 @@ class OCRService:
     def _image_preprocess(img: Image.Image) -> Image.Image:
         if img.mode != 'RGB':
             img = img.convert('RGB')
-        # Limit maximum size to 960*960
-        if img.size[0] > 960 or img.size[1] > 960:
-            img.thumbnail((960, 960), Image.Resampling.LANCZOS)
-        return img
+        if img.size[0] > 1024 or img.size[1] > 1024:
+            img.thumbnail((1024, 1024), Image.Resampling.LANCZOS)
+        new_img = Image.new('RGB', (1024, 1024), (0, 0, 0))
+        new_img.paste(img, ((1024 - img.size[0]) // 2, (1024 - img.size[1]) // 2))
+        return new_img
 
     def ocr_interface(self, img: Image.Image, need_preprocess=True) -> str:
         pass
@@ -36,6 +37,13 @@ class EasyPaddleOCRService(OCRService):
                                                 devices=self._device,
                                                 warmup_size=(960, 960))
         logger.success("EasyPaddleOCR loaded successfully")
+
+    @staticmethod
+    def _image_preprocess(img: Image.Image) -> Image.Image:
+        # Optimized `easypaddleocr` doesn't require scaling preprocess
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+        return img
 
     def _easy_paddleocr_process(self, img: Image.Image) -> str:
         _, ocr_result, _ = self._paddle_ocr_module.ocr(np.array(img))
