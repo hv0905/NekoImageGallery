f285a8570ed6de39b929f7ba49be665acc592f86
pk5ls20 <pk5ls20@outlook.com>
2024-04-28 14:32:06 +0800

    feat: Prefer to use the ImageData's format field as the extension name

--- app/Controllers/search.py
+++ app/Controllers/search.py
@@ -24,7 +24,7 @@ searchRouter = APIRouter(dependencies=([Depends(force_access_token_verify)] if c
 async def result_postprocessing(resp: SearchApiResponse) -> SearchApiResponse:
     for item in resp.result:
         if item.img.local and config.storage.method.enabled:
-            img_extension = item.img.url.split('.')[-1]
+            img_extension = item.img.format or item.img.url.split('.')[-1]
             img_remote_filename = f"{item.img.id}.{img_extension}"
             item.img.url = await storage_service.active_storage.presign_url(img_remote_filename)
             if item.img.thumbnail_url is not None:
