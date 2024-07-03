e8430cc7eb8ffc91ed5f37082cf2a4945c8d732c
pk5ls20 <pk5ls20@outlook.com>
2024-07-03 09:14:21 +0800

    Fix the lifespan function of the StorageService

--- app/Services/storage/__init__.py
+++ app/Services/storage/__init__.py
@@ -1,4 +1,6 @@
 from app.Services.lifespan_service import LifespanService
+from app.Services.storage.base import BaseStorage
+from app.Services.storage.disabled_storage import DisabledStorage
 from app.Services.storage.local_storage import LocalStorage
 from app.Services.storage.s3_compatible_storage import S3Storage
 from app.config import config, StorageMode
@@ -6,22 +8,20 @@ from app.config import config, StorageMode
 
 class StorageService(LifespanService):
     def __init__(self):
-        self.local_storage = LocalStorage()
         self.active_storage = None
         match config.storage.method:
             case StorageMode.LOCAL:
-                self.active_storage = self.local_storage
+                self.active_storage = LocalStorage()
             case StorageMode.S3:
                 self.active_storage = S3Storage()
             case StorageMode.DISABLED:
-                return
+                self.active_storage = DisabledStorage()
             case _:
                 raise NotImplementedError(f"Storage method {config.storage.method} not implemented. "
                                           f"Available methods: local, s3")
 
     async def on_load(self):
-        await self.active_storage.on_load() if self.active_storage else await self.local_storage.on_load()
+        await self.active_storage.on_load()
 
     async def on_exit(self):
-        await self.active_storage.on_exit() if self.active_storage else await self.local_storage.on_exit()
-
+        await self.active_storage.on_exit()
