0c3a34d28c958ae11fcc3488a8e1049da6d38876
EdgeNeko <edgeneko@aiursoft.com>
2024-07-12 22:35:06 +0800

    Convert field description to annotated format for MappedImage

--- app/Models/mapped_image.py
+++ app/Models/mapped_image.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Annotated
 from uuid import UUID
 
 from numpy import ndarray
@@ -9,29 +9,30 @@ from pydantic import BaseModel, Field, ConfigDict
 class MappedImage(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True, extra='ignore')
 
-    id: UUID = Field(description="The unique ID of the image. The ID is generated from the digest of the image.")
-    url: Optional[str] = Field(default=None,
-                               description="The URL of the image. For non-local images, this is specified by uploader.")
-    thumbnail_url: Optional[str] = Field(default=None,
-                                         description="The URL of the thumbnail image. For non-local thumbnails, "
-                                                     "this is specified by uploader.")
-    ocr_text: Optional[str] = Field(default=None,
-                                    description="The OCR text of the image. None if no OCR text was detected.")
-    image_vector: Optional[ndarray] = Field(None, exclude=True)
-    text_contain_vector: Optional[ndarray] = Field(None, exclude=True)
-    index_date: datetime = Field(description="The date when the image was indexed.")
-    width: Optional[int] = Field(default=None, description="The width of the image in pixels.")
-    height: Optional[int] = Field(default=None, description="The height of the image in pixels.")
-    aspect_ratio: Optional[float] = Field(default=None,
-                                          description="The aspect ratio of the image. calculated by width / height.")
-    starred: Optional[bool] = Field(default=False, description="Whether the image is starred.")
-    categories: Optional[list[str]] = Field(default=[], description="The categories of the image.")
-    local: Optional[bool] = Field(default=False,
-                                  description="Whether the image is stored in local storage.(local image).")
-    local_thumbnail: Optional[bool] = Field(default=False,
-                                            description="Whether the thumbnail image is stored in local storage.")
+    id: Annotated[
+        UUID, Field(description="The unique ID of the image. The ID is generated from the digest of the image.")] = None
+    url: Annotated[Optional[str], Field(
+        description="The URL of the image. For non-local images, this is specified by uploader.")] = None
+    thumbnail_url: Annotated[Optional[str], Field(
+        description="The URL of the thumbnail image. For non-local thumbnails, "
+                    "this is specified by uploader.")] = None
+    ocr_text: Annotated[
+        Optional[str], Field(description="The OCR text of the image. None if no OCR text was detected.")] = None
+    image_vector: Annotated[Optional[ndarray], Field(exclude=True)] = None
+    text_contain_vector: Annotated[Optional[ndarray], Field(exclude=True)] = None
+    index_date: Annotated[datetime, Field(description="The date when the image was indexed.")]
+    width: Annotated[Optional[int], Field(description="The width of the image in pixels.")] = None
+    height: Annotated[Optional[int], Field(description="The height of the image in pixels.")] = None
+    aspect_ratio: Annotated[Optional[float], Field(
+        description="The aspect ratio of the image. calculated by width / height.")] = None
+    starred: Annotated[Optional[bool], Field(description="Whether the image is starred.")] = False
+    categories: Annotated[Optional[list[str]], Field(description="The categories of the image.")] = []
+    local: Annotated[Optional[bool], Field(
+        description="Whether the image is stored in local storage.(local image)")] = False
+    local_thumbnail: Annotated[Optional[bool], Field(
+        description="Whether the thumbnail image is stored in local storage.")] = False
     format: Optional[str] = None  # required for s3 local storage
-    comments: Optional[str] = Field(default=None, description="Any custom comments or text payload for the image.")
+    comments: Annotated[Optional[str], Field(description="Any custom comments or text payload for the image.")] = None
 
     @property
     def ocr_text_lower(self) -> str | None:
